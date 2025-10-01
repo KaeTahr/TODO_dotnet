c3e10ef71af11d81f6b1f9f2883e975bf3d34878
kaetahr <lostech@protonmail.com>
2025-10-01 14:18:05 +1000

    toolbar

--- todo-frontend/src/app/app.ts
+++ todo-frontend/src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { TodoList } from './todo-list/todo-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TodoList],
-  template: `<app-todo-list></app-todo-list>`,
+  imports: [TodoList, MatToolbarModule, MatIconModule],
+  template: `
+  <mat-toolbar color="primary" class="app-header">
+    <mat-icon class="logo">checklist</mat-icon>
+    <span class="title">My Todo App</span>
+    <span class="spacer"></span>
+    <button mat-icon-button aria-label="Account">
+      <mat-icon>account_circle</mat-icon>
+    </button>
+  </mat-toolbar>
+  <app-todo-list></app-todo-list>`,
   styleUrls: ['./app.scss']
 })
 export class App {
